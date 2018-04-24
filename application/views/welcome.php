803ebb7a9049487bc6e1ac8b53d1e4ba195f68c3
PNC\Chantha.ROEURN <chantha.roeurn@student.passerellesnumeriques.org>
2018-04-24 15:44:55 +0700

    I change on welcome.php

--- application/views/welcome.php
+++ application/views/welcome.php
@@ -1,34 +1,150 @@
-<div id="container">
-	<div class="row-fluid">
-		<div class="col-12">
-	<h1>Welcome to CodeIgniter skeleton application</h1>
 
-		<p>The Skeleton application already contains everything for user and session management:</p>
-		<ul>
-			<li><a href="<?php echo base_url();?>connection/login">Login page</a>.</li>
-			<li><a href="<?php echo base_url();?>users/index">List of users (you need to be logged in)</a>.</li>
-		</ul>
-		<p>The Skeleton application contains a lot of <a href="<?php echo base_url();?>examples/views/index">examples for JS</a>:</p>
-		<ul>
-			<li>A ton of icons</li>
-			<li>A date picker</li>
-			<li>A calendar</li>
-			<li>Some charts</li>
-			<li>A JS lib to manipulate dates</li>
-			<li>An autocomplete field</li>
-			<li>Two rich text editors</li>
-			<li>A treeview widget</li>
-		</ul>
-		<p>The Skeleton application contains a lot of <a href="<?php echo base_url();?>examples/views/index">examples for PHP</a>:</p>
-		<ul>
-			<li>Create an Excel file (See list of users / export)</li>
-			<li>Create a barcode</li>
-			<li>Create a PDF file</li>
-			<li>Create charts as an image</li>
-		</ul>
+   <style>
+   .sidenav {
+    height: 100%;
+    width: 200px;
+    position: fixed;
+    z-index: 1;
+    top: 40px;
+    left: 0;
+    background-color:pink;
+    overflow-x: hidden;
+    padding-top: 20px;
+    border-right:solid green 2px;
+	}
 
-		<p>If you are exploring CodeIgniter for the very first time, you should start by reading the <a target="_blank" href="user_guide/">User Guide</a>.</p>
+	.sidenav a {
+	    padding: 6px 8px 6px 16px;
+	    text-decoration: none;
+	    font-size: 16px;
+	    color: #818181;
+	    display: block;
+	}
 
+	.sidenav a:hover {
+	    color: #f1f1f1;
+	}
+
+	.main {
+	    font-size: 18px; /* Increased text to enable scrolling */
+	}
+
+	/*sidena2*/
+	 .sidenav2 {
+	    height: 100%;
+	    width: 200px;
+	    position: fixed;
+	    z-index: 1;
+	    top: 40px;
+	    right: 0;
+	    background-color:pink;
+	    overflow-x: hidden;
+	    padding-top: 20px;
+	    border-left:solid green 2px;
+		}
+
+	.sidenav2 a {
+	    padding: 6px 8px 6px 16px;
+	    text-decoration: none;
+	    font-size: 14px;
+	    color: purple;
+	    display: block;
+	}
+
+	.sidenav2 a:hover {
+	    color: #f1f1f1;
+	}
+	li{
+		text-decoration:none;
+		list-style-type: none;
+	}
+	
+	img{
+		width: 249px;
+		height: 200px;
+		float: left;
+		padding: 10px;
+	}
+.material-icons{
+    display: inline-flex;
+    vertical-align: top;
+}
+</style>
+
+
+
+
+	<div class="container">
+		<div class="row">
+			<div class="col-md-3">
+				<div class="sidenav">
+				  <a href="#about"><i class="mdi mdi-rice"></i>&nbsp; Try foot</a>
+				  <a href="#about"><i class="mdi mdi-rice"></i>&nbsp; Water food</a>
+				  <a href="#about"><i class="mdi mdi-rice"></i>&nbsp;  Other</a>
+				</div>
+			</div>
+		
+			<div class="col-md-6">		
+				  <br>
+				  <i style="margin-left:-10px; color:purple;" class="mdi mdi-account-circle"></i><span>&nbsp; Canteen Manage</span>
+				  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nisi impedit non reprehenderit temporibus nostrum laborum deleniti, consectetur. Magnam earum asperiores harum eveniet rerum cupiditate, suscipit quibusdam iusto accusamus alias tenetur.</p>
+				
+				 <div class="card">
+				    <!-- <div class="card-header">Header</div> -->
+				    <div class="card-body">
+				    	<img src="<?php echo base_url();  ?>assets/images/food/food1.jpg" alt="">
+					<img src="<?php echo base_url();  ?>assets/images/food/food2.jpg" alt="">
+				    </div> 
+				    <div class="card-footer">
+				    	<div class="container">
+				    		<div class="row">
+				    			<div class="col-md-4"><i class="mdi mdi-thumb-up"></i>&nbsp; Interest</div>
+						    	<div class="col-md-4"><i class="mdi mdi-rice"></i>&nbsp; Order</div>
+						    	<div class="col-md-4"><i class="mdi mdi-comment"></i>&nbsp; Recomment</div>
+				    		</div>
+				    	</div>
+				    	
+				    </div>
+				 </div>
+				 <br>
+				   <div class="user-comment" style="margin-left: 80px;">
+				    	 <i style="margin-left:-10px; color:blue;" class="mdi mdi-account-circle">User</i><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellat repudiandae laudantium pariatur consequatur officia incidunt, dolore architecto eaque, </p>
+				    	 
+				    	 <i style="margin-left:-10px; color:blue;" class="mdi mdi-account-circle">User</i><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellat repudiandae laudantium pariatur consequatur officia incidunt, dolore architecto eaque, </p>
+
+				    </div>
+				    <div class="manage-comment">
+				    	<i style="margin-left:-10px; color:purple;" class="mdi mdi-account-circle"></i><span>&nbsp; Canteen Manage</span><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Autem corporis nihil, illo error eos suscipit placeat sed consequuntur aliquam,</p>
+				 	
+				    </div>
+			</div>
+
+			<div class="col-md-3">
+				<div class="sidenav2">
+					<ul>
+						<li>
+							<i style="margin-left:-30px;" class="mdi mdi-account-circle"></i> <span>&nbsp; User login now</span>
+							
+						</li>
+						<li>
+							 <i style="margin-left:-10px;" class="mdi mdi-account-circle"></i><span>&nbsp; Davy</span>
+						</li>
+						<li>
+							 <i style="margin-left:-10px;" class="mdi mdi-account-circle"></i><span>&nbsp; Chantha</span>
+						</li>
+						<li>
+							 <i style="margin-left:-10px;" class="mdi mdi-account-circle"></i><span>&nbsp; Khai</span>
+						</li>
+						<li>
+							 <i style="margin-left:-10px;" class="mdi mdi-account-circle"></i><span>&nbsp; Kemseong</span>
+						</li>
+					</ul>
+				</div>
+			</div>
 		</div>
 	</div>
-</div>
+
+
+
+     
+ 
