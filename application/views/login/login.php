dac4090947144db2d96de6c0773a27e3d70f1403
PNC\Kimsoeng.KAO <kimsoeng.kao@student.passerellesnumeriques.org>
2018-04-24 13:16:02 +0700

    edit on form login.php create style.css file add an image in assets/images/background.jpg

--- application/views/login/login.php
+++ application/views/login/login.php
@@ -1,10 +1,20 @@
 
-<div class="container">
+<style>
+  body {
+    background: linear-gradient(45deg, rgba(255,255,255, 0.2), 
+   rgba(43, 139, 227, 0.85)), 
+   url('images/background.jpg') no-repeat center bottom;
+   background-repeat: no-repeat;
+   background-size: cover;
+  }
+</style>
+<div class="container login">
 <?php echo $flashPartialView;?>
   <?php
   $attributes = array('id' => 'formLogin', 'class' => 'form-signin');
   echo form_open('connection/login', $attributes); ?>
-    <h2 class="text-center">Please Sing In</h2>
+    <!-- <h1 class="text-center">Please Sign In</h1> -->
+    <br>
     <div class="row">
       <div class="col-sm-3"></div>
       <div class="col-sm-6 jumbotron">
@@ -17,7 +27,8 @@
               <div class="form-group">
                <input type="password" name="password" class="form-control" placeholder="Password" required>
               </div>
-               <button class="btn btn-outline-info btn-block" type="submit">Sign in</button>
+               <button class="btn btn-outline-info float-right " type="submit">Sign in</button>
+               <a href="<?php echo base_url() ?>" class="btn btn-outline-danger ">Cancle</a>
                 <?php echo validation_errors() ?>
             </form>
           </div>
