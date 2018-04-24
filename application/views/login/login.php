466885e3643e97bd2c2cfe9a88ec3a8c0e3fc871
PNC\Kimsoeng.KAO <kimsoeng.kao@student.passerellesnumeriques.org>
2018-04-24 16:50:39 +0700

    add form login.php background "kimsoeng kao"

--- application/views/login/login.php
+++ application/views/login/login.php
@@ -3,9 +3,11 @@
   body {
     background: linear-gradient(45deg, rgba(255,255,255, 0.2), 
    rgba(43, 139, 227, 0.85)), 
-   url('images/background.jpg') no-repeat center bottom;
-   background-repeat: no-repeat;
+   url('<?php echo base_url();?>assets/images/background.jpg') no-repeat center bottom;
    background-size: cover;
+  }
+  .login {
+    margin-top: 10%;
   }
 </style>
 <div class="container login">
@@ -13,21 +15,19 @@
   <?php
   $attributes = array('id' => 'formLogin', 'class' => 'form-signin');
   echo form_open('connection/login', $attributes); ?>
-    <!-- <h1 class="text-center">Please Sign In</h1> -->
     <br>
     <div class="row">
       <div class="col-sm-3"></div>
       <div class="col-sm-6 jumbotron">
         <div class="card bg-default ">
           <div class="card-body">
-            <form action="<?php echo base_url() ?>c_admin/login" method="post">
               <div class="form-group">
                 <input type="text" name="login" class="form-control" placeholder="Login" required autofocus>
               </div>
               <div class="form-group">
                <input type="password" name="password" class="form-control" placeholder="Password" required>
               </div>
-               <button class="btn btn-outline-info float-right " type="submit">Sign in</button>
+               <button class="btn btn-outline-info float-right " type="submit" id="login">Sign in</button>
                <a href="<?php echo base_url() ?>" class="btn btn-outline-danger ">Cancle</a>
                 <?php echo validation_errors() ?>
             </form>
