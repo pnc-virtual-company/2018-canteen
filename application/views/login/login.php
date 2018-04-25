43889121854a863d3e3a813872fa418c90da04ce
PNC\Kimsoeng.KAO <kimsoeng.kao@student.passerellesnumeriques.org>
2018-04-25 20:18:26 +0700

    confige on login with ajax by "kimsoeng kao"

--- application/views/login/login.php
+++ application/views/login/login.php
@@ -2,32 +2,33 @@
 <style>
   body {
     background: linear-gradient(45deg, rgba(255,255,255, 0.2), 
-   rgba(43, 139, 227, 0.85)), 
-   url('<?php echo base_url();?>assets/images/background.jpg') no-repeat center bottom;
-   background-size: cover;
+    rgba(0, 0, 0,0.6)), 
+    url('<?php echo base_url();?>assets/images/background.jpg') no-repeat center bottom;
+    background-size: cover;
   }
-  .login {
-    margin-top: 10%;
+  #canteen-logo {
+    width: 25%;
+    margin-left:38%;
   }
 </style>
-<div class="container login">
+<div class="container">
 <?php echo $flashPartialView;?>
   <?php
   $attributes = array('id' => 'formLogin', 'class' => 'form-signin');
   echo form_open('connection/login', $attributes); ?>
-    <br>
+    <img src="<?php echo base_url();?>assets/images/canteen-logo.png" alt="canteen-logo" id="canteen-logo" class="text-center">
     <div class="row">
       <div class="col-sm-3"></div>
       <div class="col-sm-6 jumbotron">
         <div class="card bg-default ">
           <div class="card-body">
               <div class="form-group">
-                <input type="text" name="login" class="form-control" placeholder="Login" required autofocus>
+                <input type="text" name="login" class="form-control" placeholder="Login" required autofocus id="login">
               </div>
               <div class="form-group">
-               <input type="password" name="password" class="form-control" placeholder="Password" required>
+               <input type="password" name="password" class="form-control" placeholder="Password" required id="password">
               </div>
-               <button class="btn btn-outline-info float-right " type="submit" id="login">Sign in</button>
+               <button class="btn btn-outline-info float-right " type="submit" id="btn-login">Sign in</button>
                <a href="<?php echo base_url() ?>" class="btn btn-outline-danger ">Cancle</a>
                 <?php echo validation_errors() ?>
             </form>
@@ -37,7 +38,6 @@
       <div class="col-sm-3"></div>
     </div>
   </div>
-
 </div> <!-- /container -->
 
 <script>
