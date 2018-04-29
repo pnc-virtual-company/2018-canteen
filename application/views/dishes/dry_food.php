c25d2aee18973a7c781db376fe3c226399ab462b
PNC\Sun.MEAS <sun.meas@student.passerellesnumeriques.org>
2018-04-29 15:22:24 +0700

    change files as dry_food.php, water_food.php, index.php, welcome.php SUNMEAS

--- application/views/dishes/dry_food.php
+++ application/views/dishes/dry_food.php
@@ -1,9 +1,484 @@
+<style>
+	
+	.thumbnail {
+	  position: relative;
+	  width: 100%;
+	}
 
-<div class="row">
-	<div class="col-md-3">
-		
-	</div>
-		<div class="col-md-3">
-		<h2>this is dry food</h2>
+	.overlay {
+	  position: absolute;
+	  bottom: 100%;
+	  left: 0;
+	  right: 0;
+	  background-color: rgba(34, 40, 44, 0.56);
+	  overflow: hidden;
+	  width: 100%;
+	  height:0;
+	  transition: .5s ease;
+	}
+
+	.thumbnail:hover .overlay {
+	  bottom: 0;
+	  height: 100%;
+	}
+
+	.text {
+	  color: white;
+	  font-size: 20px;
+	  position: absolute;
+	  top: 50%;
+	  left: 50%;
+	  transform: translate(-50%, -50%);
+	  -ms-transform: translate(-50%, -50%);
+	  text-align: center;
+	}
+</style>
+<div class="container">
+	<hr>
+		<div class="header text-center">
+			<h1>Lists all dry foods</h1>
+		</div>
+		<div class="row">
+			<div class="col-md-2"></div>
+			<div class="col-md-4">
+				<div class="card">
+					<div class="card-body">
+						<h4 style="text-align: center;">Mixxed vegetables</h4>
+					<div class="thumbnail">
+						<a href="<?php echo base_url();?>assets/images/food1.jpg" target="_blank">
+						<img src="<?php echo base_url();?>assets/images/food1.jpg" alt="Fjords" style="width:100%">
+						<div class="overlay">
+							<div class="text">Dry food Description</div>
+						</div>
+						</a>
+					</div>
+					</div>
+				</div>
+			</div>
+
+			<div class="col-md-4">
+				<div class="card">
+					<div class="card-body">
+						<h4 style="text-align: center;">Mixxed vegetables</h4>
+					<div class="thumbnail">
+						<a href="<?php echo base_url();?>assets/images/food1.jpg" target="_blank">
+						<img src="<?php echo base_url();?>assets/images/food1.jpg" alt="Fjords" style="width:100%">
+						<div class="overlay">
+							<div class="text">Dry food Description</div>
+						</div>
+						</a>
+					</div>
+					</div>
+				</div>
+			</div>
+			<div class="col-md-2"></div>
+
+			<div class="col-md-2"></div>
+			<div class="col-md-4">
+				<div class="card">
+					<div class="card-body">
+						<h4 style="text-align: center;">Mixxed vegetables</h4>
+					<div class="thumbnail">
+						<a href="<?php echo base_url();?>assets/images/food1.jpg" target="_blank">
+						<img src="<?php echo base_url();?>assets/images/food1.jpg" alt="Fjords" style="width:100%">
+						<div class="overlay">
+							<div class="text">Dry food Description</div>
+						</div>
+						</a>
+					</div>
+					</div>
+				</div>
+			</div>
+
+			<div class="col-md-4">
+				<div class="card">
+					<div class="card-body">
+						<h4 style="text-align: center;">Mixxed vegetables</h4>
+					<div class="thumbnail">
+						<a href="<?php echo base_url();?>assets/images/food1.jpg" target="_blank">
+						<img src="<?php echo base_url();?>assets/images/food1.jpg" alt="Fjords" style="width:100%">
+						<div class="overlay">
+							<div class="text">Dry food Description</div>
+						</div>
+						</a>
+					</div>
+					</div>
+				</div>
+			</div>
+			<div class="col-md-2"></div>
+			
+			<div class="col-md-2"></div>
+			<div class="col-md-4">
+				<div class="card">
+					<div class="card-body">
+						<h4 style="text-align: center;">Mixxed vegetables</h4>
+					<div class="thumbnail">
+						<a href="<?php echo base_url();?>assets/images/food1.jpg" target="_blank">
+						<img src="<?php echo base_url();?>assets/images/food1.jpg" alt="Fjords" style="width:100%">
+						<div class="overlay">
+							<div class="text">Dry food Description</div>
+						</div>
+						</a>
+					</div>
+					</div>
+				</div>
+			</div>
+
+			<div class="col-md-4">
+				<div class="card">
+					<div class="card-body">
+						<h4 style="text-align: center;">Mixxed vegetables</h4>
+					<div class="thumbnail">
+						<a href="<?php echo base_url();?>assets/images/food1.jpg" target="_blank">
+						<img src="<?php echo base_url();?>assets/images/food1.jpg" alt="Fjords" style="width:100%">
+						<div class="overlay">
+							<div class="text">Dry food Description</div>
+						</div>
+						</a>
+					</div>
+					</div>
+				</div>
+			</div>
+			<div class="col-md-2"></div>
+			
+			<div class="col-md-2"></div>
+			<div class="col-md-4">
+				<div class="card">
+					<div class="card-body">
+						<h4 style="text-align: center;">Mixxed vegetables</h4>
+					<div class="thumbnail">
+						<a href="<?php echo base_url();?>assets/images/food1.jpg" target="_blank">
+						<img src="<?php echo base_url();?>assets/images/food1.jpg" alt="Fjords" style="width:100%">
+						<div class="overlay">
+							<div class="text">Dry food Description</div>
+						</div>
+						</a>
+					</div>
+					</div>
+				</div>
+			</div>
+
+			<div class="col-md-4">
+				<div class="card">
+					<div class="card-body">
+						<h4 style="text-align: center;">Mixxed vegetables</h4>
+					<div class="thumbnail">
+						<a href="<?php echo base_url();?>assets/images/food1.jpg" target="_blank">
+						<img src="<?php echo base_url();?>assets/images/food1.jpg" alt="Fjords" style="width:100%">
+						<div class="overlay">
+							<div class="text">Dry food Description</div>
+						</div>
+						</a>
+					</div>
+					</div>
+				</div>
+			</div>
+			<div class="col-md-2"></div>
+			
+			<div class="col-md-2"></div>
+			<div class="col-md-4">
+				<div class="card">
+					<div class="card-body">
+						<h4 style="text-align: center;">Mixxed vegetables</h4>
+					<div class="thumbnail">
+						<a href="<?php echo base_url();?>assets/images/food1.jpg" target="_blank">
+						<img src="<?php echo base_url();?>assets/images/food1.jpg" alt="Fjords" style="width:100%">
+						<div class="overlay">
+							<div class="text">Dry food Description</div>
+						</div>
+						</a>
+					</div>
+					</div>
+				</div>
+			</div>
+
+			<div class="col-md-4">
+				<div class="card">
+					<div class="card-body">
+						<h4 style="text-align: center;">Mixxed vegetables</h4>
+					<div class="thumbnail">
+						<a href="<?php echo base_url();?>assets/images/food1.jpg" target="_blank">
+						<img src="<?php echo base_url();?>assets/images/food1.jpg" alt="Fjords" style="width:100%">
+						<div class="overlay">
+							<div class="text">Dry food Description</div>
+						</div>
+						</a>
+					</div>
+					</div>
+				</div>
+			</div>
+			<div class="col-md-2"></div>
+			
+			<div class="col-md-2"></div>
+			<div class="col-md-4">
+				<div class="card">
+					<div class="card-body">
+						<h4 style="text-align: center;">Mixxed vegetables</h4>
+					<div class="thumbnail">
+						<a href="<?php echo base_url();?>assets/images/food1.jpg" target="_blank">
+						<img src="<?php echo base_url();?>assets/images/food1.jpg" alt="Fjords" style="width:100%">
+						<div class="overlay">
+							<div class="text">Dry food Description</div>
+						</div>
+						</a>
+					</div>
+					</div>
+				</div>
+			</div>
+
+			<div class="col-md-4">
+				<div class="card">
+					<div class="card-body">
+						<h4 style="text-align: center;">Mixxed vegetables</h4>
+					<div class="thumbnail">
+						<a href="<?php echo base_url();?>assets/images/food1.jpg" target="_blank">
+						<img src="<?php echo base_url();?>assets/images/food1.jpg" alt="Fjords" style="width:100%">
+						<div class="overlay">
+							<div class="text">Dry food Description</div>
+						</div>
+						</a>
+					</div>
+					</div>
+				</div>
+			</div>
+			<div class="col-md-2"></div>
+			
+			<div class="col-md-2"></div>
+			<div class="col-md-4">
+				<div class="card">
+					<div class="card-body">
+						<h4 style="text-align: center;">Mixxed vegetables</h4>
+					<div class="thumbnail">
+						<a href="<?php echo base_url();?>assets/images/food1.jpg" target="_blank">
+						<img src="<?php echo base_url();?>assets/images/food1.jpg" alt="Fjords" style="width:100%">
+						<div class="overlay">
+							<div class="text">Dry food Description</div>
+						</div>
+						</a>
+					</div>
+					</div>
+				</div>
+			</div>
+
+			<div class="col-md-4">
+				<div class="card">
+					<div class="card-body">
+						<h4 style="text-align: center;">Mixxed vegetables</h4>
+					<div class="thumbnail">
+						<a href="<?php echo base_url();?>assets/images/food1.jpg" target="_blank">
+						<img src="<?php echo base_url();?>assets/images/food1.jpg" alt="Fjords" style="width:100%">
+						<div class="overlay">
+							<div class="text">Dry food Description</div>
+						</div>
+						</a>
+					</div>
+					</div>
+				</div>
+			</div>
+			<div class="col-md-2"></div>
+			
+			<div class="col-md-2"></div>
+			<div class="col-md-4">
+				<div class="card">
+					<div class="card-body">
+						<h4 style="text-align: center;">Mixxed vegetables</h4>
+					<div class="thumbnail">
+						<a href="<?php echo base_url();?>assets/images/food1.jpg" target="_blank">
+						<img src="<?php echo base_url();?>assets/images/food1.jpg" alt="Fjords" style="width:100%">
+						<div class="overlay">
+							<div class="text">Dry food Description</div>
+						</div>
+						</a>
+					</div>
+					</div>
+				</div>
+			</div>
+
+			<div class="col-md-4">
+				<div class="card">
+					<div class="card-body">
+						<h4 style="text-align: center;">Mixxed vegetables</h4>
+					<div class="thumbnail">
+						<a href="<?php echo base_url();?>assets/images/food1.jpg" target="_blank">
+						<img src="<?php echo base_url();?>assets/images/food1.jpg" alt="Fjords" style="width:100%">
+						<div class="overlay">
+							<div class="text">Dry food Description</div>
+						</div>
+						</a>
+					</div>
+					</div>
+				</div>
+			</div>
+			<div class="col-md-2"></div>
+			
+			<div class="col-md-2"></div>
+			<div class="col-md-4">
+				<div class="card">
+					<div class="card-body">
+						<h4 style="text-align: center;">Mixxed vegetables</h4>
+					<div class="thumbnail">
+						<a href="<?php echo base_url();?>assets/images/food1.jpg" target="_blank">
+						<img src="<?php echo base_url();?>assets/images/food1.jpg" alt="Fjords" style="width:100%">
+						<div class="overlay">
+							<div class="text">Dry food Description</div>
+						</div>
+						</a>
+					</div>
+					</div>
+				</div>
+			</div>
+
+			<div class="col-md-4">
+				<div class="card">
+					<div class="card-body">
+						<h4 style="text-align: center;">Mixxed vegetables</h4>
+					<div class="thumbnail">
+						<a href="<?php echo base_url();?>assets/images/food1.jpg" target="_blank">
+						<img src="<?php echo base_url();?>assets/images/food1.jpg" alt="Fjords" style="width:100%">
+						<div class="overlay">
+							<div class="text">Dry food Description</div>
+						</div>
+						</a>
+					</div>
+					</div>
+				</div>
+			</div>
+			<div class="col-md-2"></div>
+			
+			<div class="col-md-2"></div>
+			<div class="col-md-4">
+				<div class="card">
+					<div class="card-body">
+						<h4 style="text-align: center;">Mixxed vegetables</h4>
+					<div class="thumbnail">
+						<a href="<?php echo base_url();?>assets/images/food1.jpg" target="_blank">
+						<img src="<?php echo base_url();?>assets/images/food1.jpg" alt="Fjords" style="width:100%">
+						<div class="overlay">
+							<div class="text">Dry food Description</div>
+						</div>
+						</a>
+					</div>
+					</div>
+				</div>
+			</div>
+
+			<div class="col-md-4">
+				<div class="card">
+					<div class="card-body">
+						<h4 style="text-align: center;">Mixxed vegetables</h4>
+					<div class="thumbnail">
+						<a href="<?php echo base_url();?>assets/images/food1.jpg" target="_blank">
+						<img src="<?php echo base_url();?>assets/images/food1.jpg" alt="Fjords" style="width:100%">
+						<div class="overlay">
+							<div class="text">Dry food Description</div>
+						</div>
+						</a>
+					</div>
+					</div>
+				</div>
+			</div>
+			<div class="col-md-2"></div>
+			
+			<div class="col-md-2"></div>
+			<div class="col-md-4">
+				<div class="card">
+					<div class="card-body">
+						<h4 style="text-align: center;">Mixxed vegetables</h4>
+					<div class="thumbnail">
+						<a href="<?php echo base_url();?>assets/images/food1.jpg" target="_blank">
+						<img src="<?php echo base_url();?>assets/images/food1.jpg" alt="Fjords" style="width:100%">
+						<div class="overlay">
+							<div class="text">Dry food Description</div>
+						</div>
+						</a>
+					</div>
+					</div>
+				</div>
+			</div>
+
+			<div class="col-md-4">
+				<div class="card">
+					<div class="card-body">
+						<h4 style="text-align: center;">Mixxed vegetables</h4>
+					<div class="thumbnail">
+						<a href="<?php echo base_url();?>assets/images/food1.jpg" target="_blank">
+						<img src="<?php echo base_url();?>assets/images/food1.jpg" alt="Fjords" style="width:100%">
+						<div class="overlay">
+							<div class="text">Dry food Description</div>
+						</div>
+						</a>
+					</div>
+					</div>
+				</div>
+			</div>
+			<div class="col-md-2"></div>
+			
+			<div class="col-md-2"></div>
+			<div class="col-md-4">
+				<div class="card">
+					<div class="card-body">
+						<h4 style="text-align: center;">Mixxed vegetables</h4>
+					<div class="thumbnail">
+						<a href="<?php echo base_url();?>assets/images/food1.jpg" target="_blank">
+						<img src="<?php echo base_url();?>assets/images/food1.jpg" alt="Fjords" style="width:100%">
+						<div class="overlay">
+							<div class="text">Dry food Description</div>
+						</div>
+						</a>
+					</div>
+					</div>
+				</div>
+			</div>
+
+			<div class="col-md-4">
+				<div class="card">
+					<div class="card-body">
+						<h4 style="text-align: center;">Mixxed vegetables</h4>
+					<div class="thumbnail">
+						<a href="<?php echo base_url();?>assets/images/food1.jpg" target="_blank">
+						<img src="<?php echo base_url();?>assets/images/food1.jpg" alt="Fjords" style="width:100%">
+						<div class="overlay">
+							<div class="text">Dry food Description</div>
+						</div>
+						</a>
+					</div>
+					</div>
+				</div>
+			</div>
+			<div class="col-md-2"></div>
+			
+			<div class="col-md-2"></div>
+			<div class="col-md-4">
+				<div class="card">
+					<div class="card-body">
+						<h4 style="text-align: center;">Mixxed vegetables</h4>
+					<div class="thumbnail">
+						<a href="<?php echo base_url();?>assets/images/food1.jpg" target="_blank">
+						<img src="<?php echo base_url();?>assets/images/food1.jpg" alt="Fjords" style="width:100%">
+						<div class="overlay">
+							<div class="text">Dry food Description</div>
+						</div>
+						</a>
+					</div>
+					</div>
+				</div>
+			</div>
+
+			<div class="col-md-4">
+				<div class="card">
+					<div class="card-body">
+						<h4 style="text-align: center;">Mixxed vegetables</h4>
+					<div class="thumbnail">
+						<a href="<?php echo base_url();?>assets/images/food1.jpg" target="_blank">
+						<img src="<?php echo base_url();?>assets/images/food1.jpg" alt="Fjords" style="width:100%">
+						<div class="overlay">
+							<div class="text">Dry food Description</div>
+						</div>
+						</a>
+					</div>
+					</div>
+				</div>
+			</div>
+
 	</div>
 </div>
