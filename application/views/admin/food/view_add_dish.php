fc25c87c9626f0e7c7a55e56070a50f624b799cd
KIMSOENG KAO <kimsoengkao1997@gmail.com>
2018-05-06 17:09:14 +0700

    [KS014] "edit on view interface of each food onf ile listDish.php" refs KS014

--- application/views/admin/food/view_add_dish.php
+++ application/views/admin/food/view_add_dish.php
@@ -3,8 +3,8 @@
 	<div class="row">
 		<div class="col-md-3"></div>
 		<div class="col-md-8" >
-			<div class="jumbotron">
-			<h2 class="text-center text-success">Add New Dish</h2>
+			<div class="jumbotron bg-info text-white">
+			<h2 class="text-center text-white">Add New Dish</h2>
 	
 			<form action="<?php echo base_url(); ?>admin/food/add_dish" enctype="multipart/form-data" method="POST">
 				
@@ -12,22 +12,19 @@
 			      <label for="email">Dish Name:</label>
 			      <input type="text" class="form-control" name="dishName" placeholder="Enter dish name " required >
 			    </div>
-
-			    <div class="form-group">
-			      <label for="pwd">Dish Image:</label>
-			      <input type="file" class="form-control" name="dishImage" required placeholder="Enter password">
-			    </div>
-
-			    <div class="form-group">
-			      <label for="pwd">Dish Description:</label>
-			      <input type="text" class="form-control" name="dishDescription" required placeholder="Enter dish's description">
-			    </div>
-
 			    <div class="form-group">
 			      <label for="pwd">Dish Date:</label>
 			      <input type="date" class="form-control" name="dishDate" required placeholder="Choose add date">
 			    </div>
-			    
+			    <div class="form-group">
+			      <label for="pwd">Dish Image:</label>
+			      <input type="file" class="form-control" name="dishImage" required placeholder="Enter password">
+			    </div>
+			    <div class="form-group">
+			      <label for="dishDescription">Dish Description:</label>
+			      <!-- <input type="text" class="form-control" name="dishDescription" required placeholder="Enter dish's description"> -->
+			      <textarea name="dishDescription" cols="30" rows="4" class="form-control"></textarea>
+			    </div>
 			    <br>
 			    <button type="button" class="btn btn-danger float-left" onclick="goBack()">Cancel</button>
 			    <button type="submit" class="btn btn-success float-right" value="upload">Add Dish</button>
