46feb421691825962b4a3c25b5c7b0a02a1e46bc
KIMSOENG KAO <kimsoengkao1997@gmail.com>
2018-05-11 16:11:07 +0700

    [KS024] "edit on dashboard welcome interface" refs KS024

--- application/views/Calendar/stuff_calendar.php
+++ application/views/Calendar/stuff_calendar.php
@@ -1,15 +1,16 @@
-<div class="app-content">
-<!--     <div class="app-title">
+ <main class="app-content">
+    <!-- <div class="app-title">
         <div>
           <h1><i class="fa fa-dashboard"></i>Create Staff Event</h1>
           <p>This application is very useful for admin and finance to manage their needs.</p>
         </div>
-  </div> -->
+  </div>
 <div class="row">
   <div class="col-2"></div>
   <div class="col-8">
     <img src="<?php echo base_url() ?>assets/images/coming-soon.png" alt="" style="width:80%">
   </div>
   <div class="col-2"></div>
-</div>
-</div>
+</div> -->
+  <img src="<?php echo base_url() ?>assets/images/coming-soon.png" alt="" style="width:70%">
+</main> 
--- application/views/dishes/breakfast.php
+++ application/views/dishes/breakfast.php
@@ -8,24 +8,27 @@
 	.card{
 		box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
 	}
+	.card {
+		margin-bottom: 15px;
+	}
 </style>
 
 <!-- Stylel css for water_food -->
 <link rel="stylesheet" href="<?php echo base_url();?>assets/css/food_type.css">
-<br>
-	<div class="header text-center">
-		<h1 class="text-info">Breakfast</h1>
+<main class="app-content">
+		<div class="app-title">
+		<div>
+			<h1>List All BreakFast</h1>
+			<p>All best food in Passerelles Numeriques Cambodai canteen</p>
+		</div>
+		<ul class="app-breadcrumb breadcrumb">
+			<li class="breadcrumb-item"><a href="<?php echo base_url() ?>Users"><span class="mdi mdi-arrow-left-bold-circle" style="font-size: 20px;"></span>&nbsp;&nbsp;Back To Welcome Board</a></li>
+		</ul>
 	</div>
-	<hr>
-<div class="container">
-	
 		<div class="row">
-
-			<div class="col-md-6 col-lg-2"></div>
 			<?php foreach ($dishes as $breakfast) {
 		?>
-			
-			<div class=" col-lg-4 col-md-4 col-sm-12 col-xs-12">
+			<div class="col-lg-4 col-md-4 col-sm-6 col-xs-12">
 				<div class="card">
 					<div class="card-body">
 						<h4 style="text-align: center;"><?php echo $breakfast->dish_name ?></h4>
@@ -33,14 +36,14 @@
 						<a href="<?php echo base_url().'assets/images/dish_uploads/'.$breakfast->dish_image ?>" target="_blank">
 						<img src="<?php echo base_url().'assets/images/dish_uploads/'.$breakfast->dish_image ?>" alt="Fjords" style="width:100% ;height : 200px;">
 						<div class="overlay">
-							<div class="text">Water food Description</div>
+							<div class="text"><?php echo $breakfast->description ?></div>
 						</div>
 						</a>
 					</div>
 					</div>
-				</div><br>
+				</div>
 			</div>
 			<?php } ?>
 		</div>	
-	</div>
+	</main>
 
