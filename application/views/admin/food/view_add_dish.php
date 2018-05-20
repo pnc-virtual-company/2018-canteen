4cf79e04272800683e5df808e635ca659a50ab2d
KIMSOENG KAO <kimsoengkao1997@gmail.com>
2018-05-20 20:18:53 +0700

    [KS032] "add commit to pull from master" refs master

--- application/views/admin/food/view_add_dish.php
+++ application/views/admin/food/view_add_dish.php
@@ -1,57 +1,55 @@
-<style>
-	.jumbotron{
-		box-shadow:  0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
-    background-color:#009688;
-	}
-</style>
-<div class="app-content">
-	  <div class="app-title">
-        <div>
-            <h1><i class="fa fa-dashboard"></i>Form to create new dish</h1>
-            <p>This application is very useful for admin and finance to manage their needs.</p>
-        </div>
-        <ul class="app-breadcrumb breadcrumb">
-          <li class="breadcrumb-item"><a href="<?php echo base_url() ?>admin/food/listDish"><span class="mdi mdi-arrow-left-bold-circle-outline" style="font-size: 20px;"></span>&nbsp;&nbsp;List of dishes</a></li>
-        </ul>
-  </div>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Bootstrap Example</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js"></script>
+</head>
+<body>
+
+
+  
+<div class="container">
+
 	<div class="row">
-		<div class="col-md-2"></div>
+		<div class="col-md-3"></div>
 		<div class="col-md-8" >
-			<div class="jumbotron  text-white">
-			<h2 class="text-center text-white">Add New Dish</h2>
+			<div class="jumbotron">
+			<h2 class="text-center text-success">Add New Dish</h2>
 	
-			<form action="<?php echo base_url(); ?>admin/food/add_dish" enctype="multipart/form-data" method="POST">
+			<form action="<?php echo base_url();?>addDish/insert_dish" enctype="multipart/form-data" method="POST">
+			
+		<!-- 	<?php //echo form_open_multipart('addDish/insert_dish');?>  -->
 				
 			    <div class="form-group">
 			      <label for="email">Dish Name:</label>
-			      <input type="text" class="form-control" name="dishName" placeholder="Enter dish name " required >
-			    </div>
-			    <div class="form-group">
-			      <label for="email">Meal Time</label>
-			      <select name="mealtime" class="form-control">
-			      	<option value="">Choose Meal Times</option>
-			      	<?php 
-			      		foreach ($mealTime as $value) {
-			       	?>
-			      	<option value="<?php echo $value->time_id ?>"><?php echo $value->name ?></option>
-			      	<?php } ?>
-			      </select>
+			      <input type="text" class="form-control" name="dishname" placeholder="Enter dish name " required >
 			    </div>
 
 			    <div class="form-group">
 			      <label for="pwd">Dish Image:</label>
-			      <input type="file" class="form-control" name="dishImage" required placeholder="Enter password">
-			       <!-- show error message when upload image -->
-			      <p><?php echo $error_msg ?></p> 
+			      <input type="file" class="form-control" name="imagename" required placeholder="Enter password">
 			    </div>
+
 			    <div class="form-group">
-			      <label for="dishDescription">Dish Description:</label>
-			      <textarea name="dishDescription" cols="30" rows="4" class="form-control"></textarea>
+			      <label for="pwd">Dish Description:</label>
+			      <input type="text" class="form-control" name="dishdescription" required placeholder="Enter dish's description">
 			    </div>
+
+			    <div class="form-group">
+			      <label for="pwd">Dish Date:</label>
+			      <input type="date" class="form-control" name="dishdate" required placeholder="Choose add date">
+			    </div>
+			    
 			    <br>
-			    <a href="<?php echo base_url() ?>admin/food/listDish" class="btn btn-danger float-left" ><i class="mdi mdi-cancel"></i>&nbsp;Cancel</a>
-			    <button type="submit" class="btn btn-warning float-right" value="upload"><i class="mdi mdi-plus-circle-outline"></i>&nbsp;Add Dish</button>
+			    <button type="submit" class="btn btn-danger float-left">Cancel</button>
+			    <button type="submit" class="btn btn-success float-right" value="upload">Add Dish</button>
 			</form>
 		</div>
 		</div>
-		<div class="col-md-2"></div>
+		<div class="col-md-1"></div>
+	 
